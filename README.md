3ababae0e725c6604f4a96027f02b850ac74a7bb
pablo <pablo@cvar>
2021-09-13 12:50:03 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# basic_mission_pixhawk_gazebo
---
+++ install_dep.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+wget https://raw.githubusercontent.com/mavlink/mavros/master/mavros/scripts/install_geographiclib_datasets.sh
+sudo bash ./install_geographiclib_datasets.sh   
+
+sudo apt-get install tmux
+sudo apt upgrade libignition-math2
