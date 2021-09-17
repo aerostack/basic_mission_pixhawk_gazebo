fc58d716f94b2771c08826218752368b02a02ba7
pablo <pablo@cvar>
2021-09-17 12:53:56 +0200

    Updated project for Aerostack 5.0

--- configs/mission/mission_pid.py
+++ configs/mission/mission_pid.py
@@ -15,8 +15,7 @@ def mission():
   mxc.executeTask('TAKE_OFF')
 
   print("FOLLOW_PATH...")
-  mxc.startTask('FOLLOW_PATH')
-  mxc.executeTask('SEND_PATH', path = [ [1, -1, 1] , [1, 1, 1] , [-1, 1, 1] , [-1, -1, 1], [0, 0, 1] ], speed = 0.5)
+  mxc.startTask('FOLLOW_PATH', path = [ [1, -1, 1] , [1, 1, 1] , [-1, 1, 1] , [-1, -1, 1], [0, 0, 1] ])
 
   print("LAND...")
   mxc.executeTask('LAND', speed = 0.3)
--- launch_gazebo.sh
+++ launch_gazebo.sh
@@ -4,7 +4,7 @@
 [ -d "/usr/share/gazebo-11" ] && export GAZEBO_RESOURCE_PATH=/usr/share/gazebo-11
 
 AEROSTACK_PROJECT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-./${AEROSTACK_PROJECT}/install_dep.sh
+${AEROSTACK_PROJECT}/install_dep.sh
 
 #SIMULATE WITHOUT CAMERA
 	#(cd $(rospack find px4) ; make px4_sitl gazebo)
@@ -14,6 +14,6 @@ AEROSTACK_PROJECT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd 
 	export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$(rospack find px4):$(rospack find px4)/Tools/sitl_gazebo
 	export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:${AEROSTACK_PROJECT}/configs/gazebo/models
 	source $(rospack find px4)/Tools/setup_gazebo.bash $(rospack find px4) $(rospack find px4)/build/px4_sitl_default
-	source $(rospack find px4)/build/px4_sitl_default/px4_sitl_default/build_gazebo/setup.sh
+	source $(rospack find px4)/build/px4_sitl_default/build_gazebo/setup.sh
 	roslaunch px4 mavros_posix_sitl.launch world:=${AEROSTACK_PROJECT}/configs/gazebo/worlds/empty.world vehicle:=iris sdf:=${AEROSTACK_PROJECT}/configs/gazebo/models/iris_cam/iris_cam.sdf respawn_gazebo:=true
 	pkill gzserver;pkill gzclient
